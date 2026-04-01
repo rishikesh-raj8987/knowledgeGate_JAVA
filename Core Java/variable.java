dab9d892195f15b253ecd3d4e974bfaa3a4827d3
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-04-01 23:57:27 +0530

    Merge branch 'main' of https://github.com/rishikesh-raj8987/knowledgeGate_JAVA

---
+++ Core Java/variable.java
@@ -0,0 +1,14 @@
+package Core Java;
+
+public class variable {
+    public static void main(String[] args) {
+        int a = 44;
+        int b=48, c=52;
+        float d =44.22f;
+        String name ="Rishi is good devloper";
+        char e='R';
+        boolean isbad= false;
+        long f = 2654574;
+        double g =14.56564d;
+    }
+}
