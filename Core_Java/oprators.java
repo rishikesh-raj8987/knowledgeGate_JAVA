0d318730f0d9a4ab9555e2ec3844e06c97f14a4a
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-04-04 23:29:49 +0530

    1

---
+++ Core_Java/oprators.java
@@ -0,0 +1,15 @@
+package Core_Java;
+
+import java.util.Scanner;
+
+public class oprators {
+    public static void main(String[] args ){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int a =sc.nextInt();
+        System.out.println("Enter the second number");
+        int b =sc.nextInt();
+        int c = a+b;
+        System.out.println("ADD =" + c);
+    }
+}
