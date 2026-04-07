040a41f7a96909a2e5f58f5f1ede9c2301f90eb9
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-04-07 23:58:28 +0530

    ternary operator

---
+++ Core_Java/ternary_operator.java
@@ -0,0 +1,11 @@
+// package Core_Java;
+
+public class ternary_operator {
+    public static void main(String[] args) {
+        
+    int n1=10 , n2 =20 ,max;
+    max = (n1>n2)? n1:n2;
+    System.out.println("maximum is = " +max);
+    }
+    
+}
