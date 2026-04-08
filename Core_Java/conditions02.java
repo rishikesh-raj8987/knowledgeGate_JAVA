be572d37c6320694f0e4c1f21875a04e65073934
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-04-08 23:31:51 +0530

    new program for check the marks of students by performance

---
+++ Core_Java/conditions02.java
@@ -0,0 +1,24 @@
+package Core_Java;
+
+import java.util.Scanner;
+
+public class conditions02 {
+    public static void main(String [] args){
+        //WAP to check the marks of the student
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the mark of Student:- ");
+        int marks = sc.nextInt();
+        if(marks>90){
+            System.out.println("Excellent");
+        } else if (marks>80 && marks <= 90) {
+            System.out.println("Good");
+        } else if (marks>70 && marks <=80) {
+            System.out.println("fair");
+        } else if (marks>60 && marks <= 70) {
+            System.out.println("Meets expectations");
+        }else{
+            System.out.println("below par");
+        }
+
+    }
+}
