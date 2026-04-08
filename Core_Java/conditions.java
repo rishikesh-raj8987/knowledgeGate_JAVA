9d9a979930b031002375052ab47e5ac36c91fe36
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-04-08 23:08:15 +0530

    new changes

--- Core_Java/conditions.java
+++ Core_Java/conditions.java
@@ -14,14 +14,16 @@ public class conditions {
 //        System.out.println("this is the answer");
 
         //question 02 -- compare two number and check which is greater
-        int n1=99;
+        int n1=9;
         int n2 = 19;
         if(n1==n2){
             System.out.println("n1 and n2 both are equal");
-        } else if (n1>n2) {
-            System.out.println("n1 is larger:- " + n1);
-        }else{
-            System.out.println("n1 and n2 both are equal");
+        }else {
+            if(n1>n2){
+                System.out.println("n1 is greater:- "+ n1);
+            }else{
+                System.out.println("n2 is larger:-"+ n2);
+            }
         }
     }
 }
