52e4e6442a6fe333c1311ca84c2cf1e8ae857da1
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-04-10 10:36:20 +0530

    new change

--- Core_Java/taking_input.java
+++ Core_Java/taking_input.java
@@ -13,24 +13,33 @@ public class taking_input {
 //            System.out.println(i);
 //        }
 //        System.out.println("done");
-        System.out.println("Enter the value of float");
-        float f =scn.nextFloat();
-        System.out.println(f);
 
-        System.out.println("Enter the long float value");
-        double d =scn.nextDouble();
-        System.out.println(d);
-
-        System.out.println("Enter the long integer value");
-        long l =scn.nextLong();
-        System.out.println(l);
-
-        System.out.println("Enter the value of String");
-        String s =scn.nextLine();
-        System.out.println(s);
-
-//        System.out.println("Enter the value of char");
-//        char c =scn.nextLine();
+//        System.out.println("Enter the value of float");
+//        float f =scn.nextFloat();
 //        System.out.println(f);
+//
+//        System.out.println("Enter the long float value");
+//        double d =scn.nextDouble();
+//        System.out.println(d);
+//
+//        System.out.println("Enter the long integer value");
+//        long l =scn.nextLong();
+//        System.out.println(l);
+//
+//        System.out.println("Enter the value of String");
+//        String name =scn.nextLine();
+//        System.out.println(name);
+
+
+
+//           when we want to take integer and string together
+        System.out.println("Enter your name and number"); // first enter the number than name
+        int n = Integer.parseInt(scn.nextLine());
+//        int n = scn.nextInt();   // direct enter the number
+        String name =scn.nextLine();
+        System.out.println("hello" + name + " counting starts");
+        for(int i=1;i<=n;i++){
+            System.out.println(i);
+        }
     }
 }
