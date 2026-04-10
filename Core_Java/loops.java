fdc13a9e4648aa5e15047dfe586f9dad6500474a
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-04-10 09:56:12 +0530

    new programme of loops

---
+++ Core_Java/loops.java
@@ -0,0 +1,12 @@
+package Core_Java;
+
+public class loops {
+    public static void main (String [] args){
+        int i=1;
+        while(i<=9){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Done");
+    }
+}
