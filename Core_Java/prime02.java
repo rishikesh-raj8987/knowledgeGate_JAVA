d67019de7f99bc2b578795b4ae91fc9d19c7807e
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-04-10 17:15:37 +0530

    program 2 of prime no

---
+++ Core_Java/prime02.java
@@ -0,0 +1,34 @@
+package Core_Java;
+
+import java.util.Scanner;
+
+public class prime02 {
+
+    public static void main(String[] args) {
+
+        // take a input how many time you want to check prime and input which number you
+        // want to check it is prime or not
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the number how many time you want to check");
+    int t = scn.nextInt();
+        for (int i = 1; i <= t; i++) {
+        System.out.println("Enter the prime number:- ");
+        int n = scn.nextInt();
+
+        int count = 0;
+        for (int div = 2; div*div <= n; div++) {
+            if (n % div == 0) {
+                count++;
+            }
+        }
+        if (count == 0) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not Prime");
+        }
+    }
+        System.out.println("Done");
+}
+}
+}
--- Core_Java/prime_no.java
+++ Core_Java/prime_no.java
@@ -3,26 +3,46 @@ package Core_Java;
 import java.util.Scanner;
 
 public class prime_no {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        // take a input how many time you want to check prime and input which number you want to check it is prime or not
+        // take a input how many time you want to check prime and input which number you
+        // want to check it is prime or not
 
-        Scanner scn =new Scanner(System.in);
+        Scanner scn = new Scanner(System.in);
         System.out.println("Enter the number how many time you want to check");
+//        int t = scn.nextInt();
+//        for (int i = 1; i <= t; i++) {
+//            System.out.println("Enter the prime number:- ");
+//            int n = scn.nextInt();
+//
+//            int count = 0;
+//            for (int div = 1; div <= n; div++) {
+//                if (n % div == 0) {
+//                    count++;
+//                }
+//            }
+//            if (count == 2) {
+//                System.out.println("Prime");
+//            } else {
+//                System.out.println("Not Prime");
+//            }
+//        }
+
+         // best time complexsity
         int t = scn.nextInt();
-        for (int i=1;i<=t;i++) {
+        for (int i = 1; i <= t; i++) {
             System.out.println("Enter the prime number:- ");
             int n = scn.nextInt();
 
-            int count=0;
-            for(int div =1;div <=n ;div++){
-                if(n % div ==0){
+            int count = 0;
+            for (int div = 2; div*div <= n; div++) {
+                if (n % div == 0) {
                     count++;
                 }
             }
-            if(count == 2){
+            if (count == 0) {
                 System.out.println("Prime");
-            }else {
+            } else {
                 System.out.println("Not Prime");
             }
         }
