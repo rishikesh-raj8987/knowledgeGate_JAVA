cebd66a5c7bf6e2cfbe5100587397f5a2230e43a
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-04-10 11:26:24 +0530

    check prime or not

--- Core_Java/prime_no.java
+++ Core_Java/prime_no.java
@@ -4,16 +4,26 @@ import java.util.Scanner;
 
 public class prime_no {
     public static void main(String[] args){
+
+        // take a input how many time you want to check prime and input which number you want to check it is prime or not
+
         Scanner scn =new Scanner(System.in);
         System.out.println("Enter the number how many time you want to check");
         int t = scn.nextInt();
-        for (int i=1;i<=t;i++){
+        for (int i=1;i<=t;i++) {
             System.out.println("Enter the prime number:- ");
             int n = scn.nextInt();
-            if(n%n==0 || n%1==0){
-                System.out.println("This number is prime number");
+
+            int count=0;
+            for(int div =1;div <=n ;div++){
+                if(n % div ==0){
+                    count++;
+                }
+            }
+            if(count == 2){
+                System.out.println("Prime");
             }else {
-                System.out.println("This is not a prime number");
+                System.out.println("Not Prime");
             }
         }
         System.out.println("Done");
