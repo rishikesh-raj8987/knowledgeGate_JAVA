105df9978d092b0922bccbadda897394be5bc945
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-04-16 23:48:30 +0530

    new programme of condtionals

---
+++ Core_Java/conditions03.java
@@ -0,0 +1,19 @@
+package Core_Java;
+
+import java.util.Scanner;
+
+public class conditions03 {
+    public static void main (String[] args){
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the number you want check");
+        int n =scn.nextInt();
+        if(n != 0){
+            if(n/3 == 0){
+                System.out.println(1);
+            } else if (n/5) {
+                
+            }
+        }
+    }
+}
