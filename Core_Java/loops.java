db6b4fdbc57a2bed85bea63cd91dc30083581f46
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-04-17 23:55:41 +0530

    new change

--- Core_Java/loops.java
+++ Core_Java/loops.java
@@ -3,16 +3,16 @@ package Core_Java;
 public class loops {
     public static void main (String [] args){
         //  while loop
-//        int i=1;
-//        while(i<=9){
-//            System.out.println(i);
-//            i++; // i= i+1;
-//        }
-//        System.out.println("Done");
+        int i=1;
+        while(i<=9){
+            System.out.println(i);
+            i++; // i= i+1;
+        }
+        System.out.println("Done");
 
         //for loop
-        for(int i=1;i<=9;i++){
-            System.out.println(i);
+        for(int j=1;j<=9;j++){
+            System.out.println(j);
         }
         System.out.println("For loop is done");
     }
