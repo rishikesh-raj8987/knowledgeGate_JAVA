4cf124b9ad5ce698653c153536032f7511ee6041
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-04-18 22:36:39 +0530

    reverse of a number

--- Core_Java/_20_reverse_Of_number.java
+++ Core_Java/_20_reverse_Of_number.java
@@ -1,4 +1,19 @@
 package Core_Java;
 
+import java.util.Scanner;
+
 public class _20_reverse_Of_number {
+    public static void main(String[] args){
+
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the number :- ");
+        int n = scn.nextInt();
+
+        while(n!=0){
+            int dig =n%10;
+             n= n/10;
+            System.out.print(dig);
+        }
+
+    }
 }
