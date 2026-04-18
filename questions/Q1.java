d3cf5ba8efb44b4458e38ebc19dd23145004203a
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-04-18 23:27:57 +0530

    new folder for question practice

---
+++ questions/Q1.java
@@ -0,0 +1,13 @@
+package questions;
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String [] args){
+        // take positive integer input and tell if it is odd or even
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the number you want to check:- ");
+        int num = scn.nextInt();
+        
+    }
+}
