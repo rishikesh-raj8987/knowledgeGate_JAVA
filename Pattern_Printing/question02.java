1562cc6282cc924a5e2cc1c84ed466837910c3ba
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-04-23 13:34:22 +0530

    Refactor pattern printing in question01.java; add question02.java for inverted pattern

--- Pattern_Printing/question02.java
+++ Pattern_Printing/question02.java
@@ -2,13 +2,13 @@ package Pattern_Printing;
 
 import java.util.Scanner;
 
-public class question01 {
+public class question02 {
     public static void main(String [] args){
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter the number:-  ");
         int n = scn.nextInt();
         for(int i = 1; i<=n;i++){
-            for(int j =1;j<=i;j++){
+            for(int j =1;j<=n-i+1;j++){
                 System.out.print("*\t");
             }
             System.out.print("\n");
