fcf2db9d93c2b2321633902c922c3499bace04bc
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-05-28 18:00:03 +0530

    Add Q7.java for checking if a number is a 4-digit integer

---
+++ questions/Q7.java
@@ -0,0 +1,11 @@
+package questions;
+
+import java.util.Scanner;
+
+public class Q7 {
+    public static void main(String[]  args ){
+        //take a postive integer input and tell it is 4 digit number or not
+        Scanner Scan = new Scanner(System.in);
+        System.out.println("Enter the number:-");
+    }
+}
