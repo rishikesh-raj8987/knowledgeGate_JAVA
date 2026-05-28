4d9967b0690adbc88efac5953ffec047b98d8a59
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-05-28 18:21:32 +0530

    Refactor Q8.java to simplify integer magnitude comparison logic and improve code readability

--- questions/Q8.java
+++ questions/Q8.java
@@ -3,24 +3,22 @@ package questions;
 import java.util.Scanner;
 
 public class Q8 {
-    public static void main(String[] args){
-        //take interger input and check it's magnitude is smaller than 69 or not
+    public static void main(String[] args) {
+
+        // Take integer input and check whether its magnitude is smaller than 69 or not
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the integer value:- ");
+
+        System.out.print("Enter the integer value: ");
         int num = scan.nextInt();
-        if(num>0){
-            if(num>69){
-                System.out.println("The integer is not smaller ");
-            }else {
-                System.out.println("The integer is smaller");
-            }
-        }else{
-            num= -num;
-            if(num>69){
-                System.out.println("The integer is not smaller ");
-            }else {
-                System.out.println("The integer is smaller");
-            }
+
+        if (num < 0) {
+            num = -num;
+        }
+
+        if (num < 69) {
+            System.out.println("The integer is smaller");
+        } else {
+            System.out.println("The integer is not smaller");
         }
     }
 }
