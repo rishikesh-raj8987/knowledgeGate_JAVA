755444ab985592b948686d44325abd6935aff5df
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-05-30 23:16:33 +0530

    Fix syntax error in UserDefineDataType.java

--- Oops/UserDefineDataType.java
+++ Oops/UserDefineDataType.java
@@ -25,6 +25,8 @@ public class UserDefineDataType {
         System.out.println(s1.name + " " + s1.rno + " " + s1.cgpa);
         s2.cgpa = 9.8 ;
         System.out.println(s2.cgpa);
+        System.out.println(s3.cgpa)
+            System.out.println(s1.cgpa)
     }
 
 }
