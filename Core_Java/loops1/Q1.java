43c7483206fe165cb5ac6500c7a6d9cf4c9eb16e
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-06-09 06:54:46 +0530

    Add Q1.java for printing numbers from 0 to 9 and create Q2.java as a placeholder

---
+++ Core_Java/loops1/Q1.java
@@ -0,0 +1,9 @@
+package Core_Java.loops;
+
+public class Q1 {
+    public static void main(String[] args) {
+        for(int i = 0;i<10;i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ Core_Java/loops1/Q2.java
@@ -0,0 +1,4 @@
+package Core_Java.loops1;
+
+public class Q2 {
+}
