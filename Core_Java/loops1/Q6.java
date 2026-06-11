6688de74289415287eba5887a2abad297598eebc
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-06-11 23:09:09 +0530

    Fix typo in comment for geometric progression display in Q6.java

--- Core_Java/loops1/Q6.java
+++ Core_Java/loops1/Q6.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Q6 {
     public static void main(String[] args) {
-        //display this GP 1 2 4 8.. upto 'n' terms
+        //display this GP 1 2 4 8.. upto 'n' terms  
         Scanner scn = new Scanner(System.in);
         System.out.println("ENter the vlaue of number :- ");
         int n = scn.nextInt();
