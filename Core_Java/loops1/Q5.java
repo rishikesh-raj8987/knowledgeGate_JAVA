d677e319ad13b320178f044c37a1ebb58b2177a8
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-06-11 17:20:56 +0530

    Refactor Q5.java to implement a new series and add Q6.java for geometric progression display

--- Core_Java/loops1/Q5.java
+++ Core_Java/loops1/Q5.java
@@ -6,11 +6,17 @@ public class Q5 {
     public static void main(String[] args) {
         //display this arithmetic progression 2,5,8,11 .....upto 'n' terms
         Scanner scn = new Scanner(System.in);
-        System.out.println("enter the value of num:- ");
-        int num = scn.nextInt();
-        for (int i=2;i<=3*num-1;i+=3){
+//        System.out.println("enter the value of num:- ");
+//        int num = scn.nextInt();
+//        for (int i=2;i<=3*num-1;i+=3){
+//            System.out.println(i);
+//        }
+
+        // print this series 99,95,91,87... upto all term which are positive
+        System.out.println("enter the value of num1:- ");
+        int num1 = scn.nextInt();
+        for (int i=99;i>=1;i-=4){
             System.out.println(i);
         }
-
     }
 }
---
+++ Core_Java/loops1/Q6.java
@@ -0,0 +1,15 @@
+package Core_Java.loops1;
+
+import java.util.Scanner;
+
+public class Q6 {
+    public static void main(String[] args) {
+        //display this GP 1 2 4 8.. upto 'n' terms
+        Scanner scn = new Scanner(System.in);
+        System.out.println("ENter the vlaue of number :- ");
+        int n = scn.nextInt();
+        for (int i = 1;i<=n;i++){
+            System.out.println();
+        }
+    }
+}
