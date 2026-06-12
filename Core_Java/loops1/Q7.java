d8f70aef6878e6edc5cc8f831aa220eaf8da0991
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-06-12 10:19:06 +0530

    Update Q7.java to print alphabets with corresponding ASCII values

--- Core_Java/loops1/Q7.java
+++ Core_Java/loops1/Q7.java
@@ -2,9 +2,9 @@ package Core_Java.loops1;
 
 public class Q7 {
     public static void main(String[] args) {
-        // print all alphabets with their correponding ASCII values.
-        for (int i=1;i<=26;i++){
-            System.out.println();
+        // print all alphabets with their correponding ASCII  values.
+        for (int i=65;i<=90;i++){
+            System.out.println((char)i + ": "+ i);
         }
     }
 }
