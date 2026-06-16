27caf5f8262a7eca6960ba3f7c0a004b282f36f5
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-06-16 17:01:18 +0530

    Add OutputInputArray.java to demonstrate array output using for and while loops

--- Arrays/Array_1.java
+++ Arrays/Array_1.java
@@ -4,5 +4,16 @@ public class Array_1 {
     public static void main(String[] args) {
         int [] a = {8,65,1,25,78,49};
         System.out.println(a[4]);
+
+        a[4]= 59;
+        System.out.println(a[4]);
+
+        int[] arr =  new int[5];
+        arr[0] = 12;
+        arr[1] = 56;
+        arr[2] = 78;
+        arr[3] = 45;
+        arr[4] = 10;
+
     }
 }
---
+++ Arrays/OutputInputArray.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class OutputInputArray {
+    public static void main(String[] args) {
+        int[] arr =  {45,-5,89,7,-14};
+        for(int i=0;i<=4;i++){
+            System.out.println(arr[i]+" ");
+        }
+        int j = 0;
+        while(j<=4){
+            System.out.print(arr[j]+" ");
+            j++;
+        }
+    }
+}
